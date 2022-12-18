808ae62e1dce9a423832e48de26d202d7d4458bf
essadeQueiros <emorim6@gmail.com>
2022-12-18 22:13:23 +0000

    frontend página signin terminado

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -22,20 +22,20 @@ const NavBar = (props) => {
         <nav className="NavBar fixed border-b-2 border-black bg-gray-700 w-full shadow-lg shadow-gray-500/100 h-auto">
             <div className="container mx-auto flex justify-center items-center p-4">
                 <div className='logo w-1/3 text-2xl'>Bank</div>  
-                <div className="flex justify-center items-center w-1/3">
+                <div className="flex justify-center items-center w-3/4 lg:w-1/3">
                     <ul className="flex w-full justify-around items-center">
-                        <li className="w-full h-full flex items-center justify-center">
-                            <Link to='/' className="group w-2/3 h-full relative flex justify-center align-center px-1 py-1 transition-all before:ease-out before:rounded-2xl before:absolute before:top-0 before:w-0 before:h-1 hover:before:w-full before:left-1/5 before:bg-green-900 before:duration-300 after:rounded after:absolute after:top-0 after:invisible hover:after:visible after:h-1 after:w-full after:duration-500 after:delay-200 hover:after:h-full before:left-1/5 after:bg-green-900">
+                        <li className="w-full h-full flex items-center lg:mx-0 mx-1 justify-center">
+                            <Link to='/' className="group w-full lg:w-2/3 h-full relative flex justify-center align-center px-1 py-1 transition-all before:ease-out before:rounded-full before:absolute before:top-0 before:w-0 before:h-1 hover:before:w-full before:left-1/5 before:bg-green-900 before:duration-300 before:border-2 before:border-white before:invisible hover:before:visible after:border-2 after:border-white after:rounded after:absolute after:top-0 after:invisible hover:after:visible after:h-1 after:w-full after:duration-500 after:delay-200 hover:after:h-full before:left-1/5 after:bg-green-900">
                                 <p className="text-white duration-500 delay-150 text-center text-lg z-20 ">Home</p>
                             </Link>
                         </li>                        
-                        <li className="w-full h-full flex items-center justify-center">
-                            <Link to='/cards' className="group w-2/3 h-full relative flex justify-center align-center px-1 py-1 transition-all before:ease-out before:rounded-2xl before:absolute before:top-0 before:w-0 before:h-1 hover:before:w-full before:left-1/5 before:bg-green-900 before:duration-300 after:rounded after:absolute after:top-0 after:invisible hover:after:visible after:h-1 after:w-full after:duration-500 after:delay-200 hover:after:h-full before:left-1/5 after:bg-green-900">
+                        <li className="w-full h-full flex items-center lg:mx-0 mx-1 justify-center">
+                            <Link to='/cards' className="group w-full lg:w-2/3 h-full relative flex justify-center align-center px-1 py-1 transition-all before:ease-out before:rounded-full before:absolute before:top-0 before:w-0 before:h-1 hover:before:w-full before:left-1/5 before:bg-green-900 before:duration-300 before:border-2 before:border-white before:invisible hover:before:visible after:border-2 after:border-white after:rounded after:absolute after:top-0 after:invisible hover:after:visible after:h-1 after:w-full after:duration-500 after:delay-200 hover:after:h-full before:left-1/5 after:bg-green-900">
                                 <p className="text-white duration-500 delay-150 text-center text-lg z-20 ">Cards</p>
                             </Link>
                         </li>
-                        <li className="w-full h-full flex items-center justify-center">
-                            <Link to='/loans' className="group w-2/3 h-full relative flex justify-center align-center px-1 py-1 transition-all before:ease-out before:rounded-2xl before:absolute before:top-0 before:w-0 before:h-1 hover:before:w-full before:left-1/5 before:bg-green-900 before:duration-300 after:rounded after:absolute after:top-0 after:invisible hover:after:visible after:h-1 after:w-full after:duration-500 after:delay-200 hover:after:h-full before:left-1/5 after:bg-green-900">
+                        <li className="w-full h-full flex items-center lg:mx-0 mx-1 justify-center">
+                            <Link to='/loans' className="group w-full lg:w-2/3 h-full relative flex justify-center align-center px-1 py-1 transition-all before:ease-out before:rounded-full before:absolute before:top-0 before:w-0 before:h-1 hover:before:w-full before:left-1/5 before:bg-green-900 before:duration-300 before:border-2 before:border-white before:invisible hover:before:visible after:border-2 after:border-white after:rounded after:absolute after:top-0 after:invisible hover:after:visible after:h-1 after:w-full after:duration-500 after:delay-200 hover:after:h-full before:left-1/5 after:bg-green-900">
                                 <p className="text-white duration-500 delay-150 text-center text-lg z-20 ">Loans</p>
                             </Link>
                         </li>
@@ -43,8 +43,8 @@ const NavBar = (props) => {
                 </div>
                 <div className='w-1/3'>
                     <Link to='/signin'>
-                        {!logged && <button className="w-24 h-10 rounded-md text-white hover:text-gray-300 bg-green-700 transition duration-100 ease-in-out border-2 hover:border-green-700 hover:bg-transparent" to='/signin'>Sign In</button>}
-                        {logged && <button className="w-24 h-10 rounded-md text-white hover:text-grays-300 bg-red-900 transition duration-100 ease-in-out border-2 hover:border-red-900 hover:bg-transparent" to='/signin'>Logout</button>}
+                        {!logged && <button className="w-24 h-10 -skew-y-4 -skew-x-6 hover:skew-y-4 hover:skew-x-6 rounded-md text-white hover:text-gray-300 bg-green-700 transition duration-100 ease-in-out border-2 hover:border-green-700 hover:bg-transparent" to='/signin'>Sign In</button>}
+                        {logged && <button className="w-24 h-10 skew-y-4 skew-x-6 hover:-skew-y-6 hover:-skew-x-12 rounded-md text-white hover:text-grays-300 bg-red-900 transition duration-100 ease-in-out border-2 hover:border-red-900 hover:bg-transparent" to='/signin'>Logout</button>}
                     </Link>
                 </div>
             </div>
--- client/src/pages/SingIn.js
+++ client/src/pages/SingIn.js
@@ -5,18 +5,14 @@ const SignIn = (props) => {
     const [openSignUp, setOpenSignUp] = useState(false)
     const [openSignIn, setOpenSignIn] = useState(true)
 
-    useEffect(() => {
-
-        console.log(openSignIn, openSignUp)
-
-    }, [openSignIn, openSignUp])
-
     const[firstName, setFirstName] = useState('')
     const[lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const [passwordWarn, setPasswordWarn] = useState(false)
+
+    const [buttonDisabled, setButtonDisabled] = useState(true)
 
     const checkPassword = (value) => {
         if(value !== password){
@@ -35,28 +31,51 @@ const SignIn = (props) => {
         setPasswordWarn(false)
     }
 
+    const checkInputs = () => {
+        console.log('Checking')
+    }
+
+    const checkEmail = () => {
+        console.log(email)
+    }
+
+    useEffect(() => {
+        if(openSignUp){
+            checkEmail()
+        }
+    }, [email])
+
     return ( 
         <div className="SignIn w-screen h-screen bg-gray-800 pt-60">
-            <div className="transition-all bg-gray-900 py-2 px-2 ease-in-out duration-300 w-5/6 mx-auto xl:w-1/3 lg:w-2/5 md:w-3/5 flex flex-col items-center justify-center border-2 border-black">
+            <div className="transition-all bg-gray-900 py-2 px-2 ease-in-out duration-300 w-5/6 mx-auto 2xl:w-1/4 xl:w-1/3 lg:w-2/5 md:w-1/2 flex flex-col items-center justify-center border-2 border-black">
                 <div className='w-full py-5 border-white border-2'>
                     <div className="flex flex-col">
                         {openSignIn ? <span className="text-3xl mb-1 text-gray-300">Sign In</span> : <span className="text-3xl mb-1 text-gray-300">Sign Up</span>}
                         {openSignIn && <span className="text-gray-400 text-md">Enter your email and password!</span>}
                     </div>
                     <div className="w-full h-full px-2 py-4 flex flex-col"> 
-                        {openSignUp && <input onChange={(e) => setFirstName(e.target.value)} value={firstName} className="mt-5 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="First Name"></input>}
-                        {openSignUp && <input onChange={(e) => setLastName(e.target.value)} value={lastName} className="my-4 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Last Name"></input>}
-                        <input onChange={(e) => setEmail(e.target.value)} value={email} className="my-4 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Email"></input>
-                        <input onChange={(e) => setPassword(e.target.value)} value={password} className="transition ease-in-out duration-200 mt-4 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Password"></input>
-                        {openSignUp && <input onChange={(e) => checkPassword(e.target.value)} className="transition ease-in-out duration-200 mt-5 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Confirm Password"></input>}
+                        {openSignUp && <input onChange={(e) => {setFirstName(e.target.value); checkInputs()}} value={firstName} className="mt-5 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="First Name"></input>}
+                        {openSignUp && <input onChange={(e) => {setLastName(e.target.value); checkInputs()}} value={lastName} className="my-4 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Last Name"></input>}
+                        <input onChange={(e) => {setEmail(e.target.value); checkInputs()}} value={email} className="my-4 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Email"></input>
+                        <input onChange={(e) => {setPassword(e.target.value); checkInputs()}} value={password} className="transition ease-in-out duration-200 mt-4 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Password"></input>
+                        {openSignUp && <div className="flex flex-col mt-2 w-full">
+                                            <span className="w-full text-md text-left text-gray-300">Password Requirements:</span>
+                                            <ul className="w-3/4  mt-2 ml-5 text-left">
+                                                <li className=" list-disc text-gray-400 text-sm">Min. 8 characters & Max. 16 characters</li>
+                                                <li className="list-disc text-gray-400 text-sm">At least 1 number (e.g., "1")</li>
+                                                <li className="list-disc text-gray-400 text-sm">At least 1 upper case letter (e.g., "A")</li>
+                                            </ul>
+                            </div>}
+                        {openSignUp && <input onChange={(e) => {checkPassword(e.target.value)}} className="transition ease-in-out duration-200 mt-5 py-1 px-2 text-white border-2 border-white rounded-lg bg-transparent outline-none" placeholder="Confirm Password"></input>}
                         {passwordWarn && <span className="mt-3 text-md text-gray-400">Passwords doesn't match</span>}
+                        
                         {openSignIn && <span className="mt-3 text-md text-gray-400">Forgot password?</span>}
                     </div>
-                    {openSignUp && <button className="mt-4 w-40 h-10 rounded text-lg text-white border-2 border-white bg-blue-600 hover:bg-transparent hover:border-2 hover:border-blue-600">Sing Up</button>}
+                    {openSignUp && <button onClick={() => {console.log('signup')}} className="mt-4 w-40 h-10 rounded text-lg text-white border-2 border-white bg-blue-600 !disabled:hover:bg-transparent !disabled:hover:border-2 !disabled:hover:border-blue-600 disabled:opacity-50">Sing Up</button>}
                     {openSignIn && <button className="mt-4 w-40 h-10 rounded text-lg text-white border-2 border-white bg-blue-600 hover:bg-transparent hover:border-2 hover:border-blue-600">Sing In</button>}
                     {openSignIn && <div className="mt-5">
                         <span className="text-gray-200">Dont have an account yet? </span>
-                        <span onClick={() => {setOpenSignUp(true); setOpenSignIn(false); resetInputs()}} className="font-bold text-blue-600 cursor-pointer">Sing Up</span>
+                        <span onClick={() => {setOpenSignUp(true); setOpenSignIn(false); resetInputs()}} className={"font-bold text-blue-600 cursor-pointer"}>Sing Up</span>
                     </div>}
                     {openSignUp && <div className="mt-5">
                         <span className="text-gray-200">Already have an account?</span>
